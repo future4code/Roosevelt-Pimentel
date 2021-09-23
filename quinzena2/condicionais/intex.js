ca23023bf2976d73480479b75288d819b585ff2e
RooseveltAndrade <rooseveltandrade14@gmail.com>
2021-09-23 18:36:36 -0300

    condicionais

---
+++ quinzena2/condicionais/intex.js
@@ -0,0 +1,69 @@
+// // Exercício 1
+
+// let primeiroNumero = Number(prompt("Digite um número"))
+// let segundoNumero = Number(prompt("Digite outro número"))
+
+// function compararNumeros( num1, num2) {
+//    if (num1 === num2) {
+//       console.log( "Sim, os números são iguais! =D")
+//    }
+// }
+
+// // compararNumeros(primeiroNumero, segundoNumero)
+
+// // Exercício 2
+
+// function compararNumerosComElse(num1, num2) {
+//    if (num1 === num2) {
+//       console.log("Os números são iguais!")
+//    } else {
+
+//       console.log("OS números são diferentes! =/")
+//    }
+// }
+// // outras lógicas...
+
+
+// compararNumerosComElse(primeiroNumero, segundoNumero)
+
+
+// let primeiroNumero = number(prompt("insira um numero"))
+// let segundoNumero = number(prompt("insira outro numero"))
+
+// function comparação(num1, num2) {
+//     if(num1 === num2) {
+//         console.log("Sim, o numero é igual")
+//     }
+//     else if(num1 > num2) {
+//         console.log("Numero 1 maior que o 2")
+//     }
+//     else (num1 < num2) {  
+//         console.log("Numero 1 menor que o 2")
+//     }
+// }
+
+// comparação(num1, num2)
+
+// const concluiuEnsinoMedio = prompt("Você concluiu o ensino medio?")
+// const temMaisDeDezoitoAnos = prompt("Você tem maior idade?")
+// const fazOutraFaculdade = prompt("Você faz outra faculdade?")
+
+// function validarCandidato(
+//     concluiuEnsinoMedio,
+//     temMaisDeDezoitoAnos,
+//     fazOutraFaculdade 
+// ) {
+    
+// if (concluiuEnsinoMedio &&
+//     temMaisDeDezoitoAnos && 
+//     fazOutraFaculdade 
+// ) {
+// console.log("pode se matricular")
+// } else {
+//     console.log("Não pode se matricular") }
+// }
+// validarCandidato(
+//     concluiuEnsinoMedio,
+//     temMaisDeDezoitoAnos,
+//     fazOutraFaculdade 
+// )
