f9d096e89f4e8af5f4e3e29af1afccd59f905ed3
RooseveltAndrade <rooseveltandrade14@gmail.com>
2021-10-01 19:56:30 -0300

    blackjack

---
+++ quinzena2/blackjack/desafio.js
@@ -0,0 +1,111 @@
+/**
+ * EXEMPLO DE UTILIZAÇÃO DA 'comprarCarta'
+ * 
+ * 
+    const carta = comprarCarta(); // Sorteia uma carta. Por exemplo, o rei de ouros
+    
+    console.log(carta.texto) // imprime o texto da carta. Exemplo: "K♦️" (indica "K" de ouros)
+    console.log(carta.valor) // imprime o valor da carta (um número). Exemplo: 10 (dado que "K" vale 10)
+ * 
+ * 
+ * 
+ */
+
+
+    let jogador = []
+    let computador = []
+    
+    if(confirm("Bem-vinde ao jogo de Blackjack!" + "\n" + "Quer iniciar uma nova rodada?")){
+    
+       let cartasOk = false
+       while (!cartasOk){
+          jogador.push(comprarCarta())
+          jogador.push(comprarCarta())
+          computador.push(comprarCarta())
+          computador.push(comprarCarta())
+          if ((jogador[0].valor === 11 && jogador[1].valor === 11) ||
+             (computador[0].valor === 11 && computador[1].valor === 11)) {
+                jogador = []
+                computador = []
+          } else {
+             cartasOk = true
+          }
+       }
+    
+       let comprando = true
+    
+       while(comprando){
+          let textos = ""
+          let pontos = 0
+          for (let carta of jogador){
+             textos += carta.texto + " "
+             pontos += carta.valor
+          }
+    
+          if (pontos > 21){
+             comprando = false
+          } else {
+             let confirmCompra = confirm(
+                `Suas cartas são ${textos}. A carta revelada do computador é ${computador[0].texto}.` +
+                "\n"+ 
+                "Deseja comprar mais uma carta?"
+             )
+          
+             if (confirmCompra){
+                jogador.push(comprarCarta())
+             } else {
+                comprando = false
+             }
+          }
+       }
+       
+       let pontosComputador = 0
+       let pontosJogador = 0
+       let textosComputador = ""
+       let textosJogador = ""
+    
+       for (let carta of computador){
+          pontosComputador += carta.valor
+          textosComputador += carta.texto + " "
+       }
+       for (let carta of jogador){
+          pontosJogador += carta.valor
+          textosJogador += carta.texto + " "
+       }
+    
+       if (pontosJogador <= 21){
+          while (pontosComputador < pontosJogador && pontosComputador <= 21){
+             computador.push(comprarCarta())
+             pontosComputador = 0
+             textosComputador = ""
+             for (let carta of computador){
+                pontosComputador += carta.valor
+                textosComputador += carta.texto + " "
+             }
+          }
+       }
+      
+       let resultado = ""
+       if (pontosJogador > pontosComputador && pontosJogador <= 21){
+          resultado = "O usuário ganhou!"
+       } else if (pontosComputador > pontosJogador && pontosComputador <= 21){
+          resultado = "O computador ganhou!"
+       } else if (pontosComputador > 21 && pontosJogador <= 21){
+          resultado = "O usuário ganhou!"
+       } else if (pontosJogador > 21 && pontosComputador <= 21){
+          resultado = "O computador ganhou!"
+       } else {
+          resultado = "Empate!"
+       }
+    
+       alert(
+          `Usuario - Cartas: ${textosJogador} - Pontuação: ${pontosJogador}` + 
+          "\n" + 
+          `Computador - Cartas: ${textosComputador} - Pontuação: ${pontosComputador}` + 
+          "\n" + 
+          resultado
+       )
+       
+    } else {
+       alert("O jogo acabou.")
+    }
--- quinzena2/funcoes-de-array/index.js
+++ quinzena2/funcoes-de-array/index.js
@@ -120,18 +120,18 @@
 
 // a) Crie um novo array que contenha apenas o nome de cada item
 
-const produtos = [
-    { nome: "Alface Lavada", categoria: "Hortifruti", preco: 2.5 },
-    { nome: "Guaraná 2l", categoria: "Bebidas", preco: 7.8 },
-    { nome: "Veja Multiuso", categoria: "Limpeza", preco: 12.6 },
-    { nome: "Dúzia de Banana", categoria: "Hortifruti", preco: 5.7 },
-    { nome: "Leite", categoria: "Bebidas", preco: 2.99 },
-    { nome: "Cândida", categoria: "Limpeza", preco: 3.30 },
-    { nome: "Detergente Ypê", categoria: "Limpeza", preco: 2.2 },
-    { nome: "Vinho Tinto", categoria: "Bebidas", preco: 55 },
-    { nome: "Berinjela kg", categoria: "Hortifruti", preco: 8.99 },
-    { nome: "Sabão em Pó Ypê", categoria: "Limpeza", preco: 10.80 }
- ]
+// const produtos = [
+//     { nome: "Alface Lavada", categoria: "Hortifruti", preco: 2.5 },
+//     { nome: "Guaraná 2l", categoria: "Bebidas", preco: 7.8 },
+//     { nome: "Veja Multiuso", categoria: "Limpeza", preco: 12.6 },
+//     { nome: "Dúzia de Banana", categoria: "Hortifruti", preco: 5.7 },
+//     { nome: "Leite", categoria: "Bebidas", preco: 2.99 },
+//     { nome: "Cândida", categoria: "Limpeza", preco: 3.30 },
+//     { nome: "Detergente Ypê", categoria: "Limpeza", preco: 2.2 },
+//     { nome: "Vinho Tinto", categoria: "Bebidas", preco: 55 },
+//     { nome: "Berinjela kg", categoria: "Hortifruti", preco: 8.99 },
+//     { nome: "Sabão em Pó Ypê", categoria: "Limpeza", preco: 10.80 }
+//  ]
 
 //      const arrayNomeDoProduto = produtos.map((item, index, array) => {
 //      return item.nome 
@@ -166,11 +166,11 @@ const produtos = [
 // e) Crie um novo array onde cada item é uma frase "Compre [NOME] por [PREÇO]". Esse
 //    array deve conter frases apenas dos itens cujo nome contenha a palavra "Ypê"
 
-let arrayFiltro = produtos.filter((item, index , array) => {
-    return item.nome.includes("Ypê") === true
-})
+// let arrayFiltro = produtos.filter((item, index , array) => {
+//     return item.nome.includes("Ypê") === true
+// })
 
-let arrayValores = produtos.map((arrayFiltro) => {
-    return "Compre" + produtos.nome + "por" + produtos.preco
-})
-console.log(arrayValores)
+// let arrayValores = produtos.map((arrayFiltro) => {
+//     return "Compre" + produtos.nome + "por" + produtos.preco
+// })
+// console.log(arrayValores)
