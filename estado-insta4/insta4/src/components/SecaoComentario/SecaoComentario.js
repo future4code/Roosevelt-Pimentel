350df2878d503075938eefde10cb63a647660f9f
RooseveltAndrade <rooseveltandrade14@gmail.com>
2021-12-02 16:25:14 -0300

    post do insta

---
+++ estado-insta4/insta4/src/components/SecaoComentario/SecaoComentario.js
@@ -0,0 +1,36 @@
+import React, {Component} from 'react'
+import styled from 'styled-components'
+
+const CommentContainer = styled.div`
+    display: flex;
+    justify-content: center;
+    padding: 5px;
+`
+
+const InputComentario = styled.input`
+    width: 100%;
+    margin-right: 5px;
+`
+
+export class SecaoComentario extends Component {
+	state = {
+
+	}
+
+	onChangeComentario() {
+
+	}
+
+	render() {
+		return <CommentContainer>
+			<InputComentario
+				placeholder={'COMENTE AQUI'}
+				const={'digite aqui'}
+				onChange={this.onChangeComentario}  
+				resultado={console.log('digite aqui')}
+			/>
+			<button onClick={this.props.aoEnviar}>Enviar</button>
+		</CommentContainer>
+
+	}
+}
