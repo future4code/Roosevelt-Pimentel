408b2a3736b04788385bf5163f78f2532dd623f2
RooseveltAndrade <rooseveltandrade14@gmail.com>
2022-01-14 21:50:07 -0300

    esta funcionando os boões porém começou a dar erro nas fotos

--- Astromatch/astroapp/src/components/match.js
+++ Astromatch/astroapp/src/components/match.js
@@ -14,6 +14,7 @@ const Box = styled.div`
   border-radius: 5px;
 `;
 
+
 const Box2 = styled.div`
 overflow: hidden;
 width: 400px;
@@ -22,6 +23,39 @@ display: flex;
 align-items: center;
 justify-content: center;
 `
+
+const Botoes = styled.div`
+width: 100% ;
+appearance: auto;
+text-rendering: auto;
+color: -internal-light-dark(black, white);
+letter-spacing: normal;
+word-spacing: normal;
+line-height: normal;
+text-transform: none;
+text-indent: 0px;
+text-shadow: none;
+display: inline-block;
+text-align: center;
+align-items: flex-start;
+cursor: pointer;
+box-sizing: border-box;
+background-color: -internal-light-dark(rgb(239, 239, 239), rgb(59, 59, 59));
+margin: 0em;
+padding: 1px 6px;
+border-width: 2px;
+border-style: outset;
+border-color: -internal-light-dark(rgb(118, 118, 118), rgb(133, 133, 133));
+border-image: initial;
+`
+const EstilizacaoBotao = styled.div`
+    display: flex;
+    justify-content: space-evenly;
+    -webkit-box-align: center;
+    align-items: center;
+    padding: 10px 0px;
+`
+
 
 function Match() {
   const [usuario, setUsuario] = useState({});
@@ -44,39 +78,36 @@ function Match() {
     pegaFotos();
   }, []);
 
-  const gostei = () => {
+  const gostei = (choice) => {
     const body = {
-      id: usuario.id,
-      choice: true
+      id: `${usuario.id}`,
+      choice: choice
     }
-    axios.post("https://us-central1-missao-newton.cloudfunctions.net/astroMatch/Roosevelt-Andrade/person")
-    body,
-      {
-        headers: {
-          Autorization: "Roosevelt-Andrade"
-        }
-      }
-        .then(res => {
-          setListMatch(res.data.profile)
-        }).catch(error => {
-          alert("erro")
-        })
-  
 
-}
+    axios.post("https://us-central1-missao-newton.cloudfunctions.net/astroMatch/Roosevelt-Andrade/choose-person", body)
+      .then((res) => {
+        pegaFotos()
+      }).catch(error => {
+        alert("erro")
+      })
+  }
 
-return (
-  <Box>
-    <Cabeca />
-    <Box2>
-      <img src={usuario.photo} />
-    </Box2>
-    <p>{usuario.bio}</p>
-    <p>{usuario.name} , {usuario.age} </p>
-    <button onClick={() => gostei(usuario.id)}>Match</button>
-    <button>X</button>
-  </Box>
-);
+  return (
+    <Box>
+      <Box2>
+        <img src={usuario.photo} />
+      </Box2>
+      <Botoes>
+        <p>{usuario.bio}</p>
+        <p>{usuario.name}  {usuario.age} </p>
+        <EstilizacaoBotao>
+        <button onClick={() => gostei(true)}>Match</button>
+        <button onClick={() => gostei(true)}>X</button>
+        </EstilizacaoBotao>
+      </Botoes>
+
+    </Box>
+  );
 }
 
 
