b32a2c1d502cea6216cab64c436192e35b87764a
RooseveltAndrade <rooseveltandrade14@gmail.com>
2022-01-15 15:52:28 -0300

    voltou as fotos mas ainda tenho erro nas funções dos botões

--- Astromatch/astroapp/src/components/match.js
+++ Astromatch/astroapp/src/components/match.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import styled from "styled-components";
-import Cabeca from "./cabeca";
+import cabeca from "./cabeca";
 
 const Box = styled.div`
   display: flex;
@@ -22,38 +22,20 @@ height: 100vh;
 display: flex;
 align-items: center;
 justify-content: center;
+margin: 0px;
+font-family: Roboto, sans-serif;
+`
+const Button1 = styled.div`
+
 `
 
-const Botoes = styled.div`
-width: 100% ;
-appearance: auto;
-text-rendering: auto;
-color: -internal-light-dark(black, white);
-letter-spacing: normal;
-word-spacing: normal;
-line-height: normal;
-text-transform: none;
-text-indent: 0px;
-text-shadow: none;
-display: inline-block;
-text-align: center;
-align-items: flex-start;
-cursor: pointer;
+const Posicao = styled.div`
+display: flex;
+justify-content: space-evenly;
+-webkit-box-align: center;
+align-items: center;
+padding: 10px 0px;
 box-sizing: border-box;
-background-color: -internal-light-dark(rgb(239, 239, 239), rgb(59, 59, 59));
-margin: 0em;
-padding: 1px 6px;
-border-width: 2px;
-border-style: outset;
-border-color: -internal-light-dark(rgb(118, 118, 118), rgb(133, 133, 133));
-border-image: initial;
-`
-const EstilizacaoBotao = styled.div`
-    display: flex;
-    justify-content: space-evenly;
-    -webkit-box-align: center;
-    align-items: center;
-    padding: 10px 0px;
 `
 
 
@@ -64,7 +46,7 @@ function Match() {
   const pegaFotos = async () => {
     await axios
       .get(
-        `https://us-central1-missao-newton.cloudfunctions.net/astroMatch/Roosevelt-Andrade/person`
+        `https://us-central1-missao-newton.cloudfunctions.net/astroMatch/Roosevelt/person`
       )
       .then((response) => {
         setUsuario(response.data.profile);
@@ -81,10 +63,10 @@ function Match() {
   const gostei = (choice) => {
     const body = {
       id: `${usuario.id}`,
-      choice: choice
+      choice: true
     }
 
-    axios.post("https://us-central1-missao-newton.cloudfunctions.net/astroMatch/Roosevelt-Andrade/choose-person", body)
+    axios.post("https://us-central1-missao-newton.cloudfunctions.net/astroMatch/Roosevelt-Andrade/person", body)
       .then((res) => {
         pegaFotos()
       }).catch(error => {
@@ -97,16 +79,18 @@ function Match() {
       <Box2>
         <img src={usuario.photo} />
       </Box2>
-      <Botoes>
-        <p>{usuario.bio}</p>
-        <p>{usuario.name}  {usuario.age} </p>
-        <EstilizacaoBotao>
-        <button onClick={() => gostei(true)}>Match</button>
-        <button onClick={() => gostei(true)}>X</button>
-        </EstilizacaoBotao>
-      </Botoes>
+      <p>{usuario.bio}</p>
+      <p>{usuario.name}  {usuario.age} </p>
+      <Posicao>
+        <Button1>
+        <button onClick={() => gostei(true)}>Dar Match</button>
+        </Button1>
+      <button onClick={() => gostei(true)}>X</button>
+      <button onClick={() => listaMatch()}>Matchs</button>
+      </Posicao>
 
     </Box>
+    
   );
 }
 
