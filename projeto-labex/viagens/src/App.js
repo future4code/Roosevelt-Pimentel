e70d8cc9f2bd2d52cdbd62cf7c6ac373c94afa6a
RooseveltAndrade <rooseveltandrade14@gmail.com>
2022-01-27 20:55:27 -0300

    pagina inicial com erro ainda

--- projeto-labex/viagens/src/App.js
+++ projeto-labex/viagens/src/App.js
@@ -1,61 +1,15 @@
 import './App.css';
-import React, { useEffect} from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import axios from 'axios';
-import useForm from './pages/useForm'; 
-
+import HomePage from "./pages/HomePage"
 
 const App = () => {
-
-  const {form, onChange, cleanFields} = useForm({ email: "" , password: ""})
-
-  // const mudaPais = (event) => {
-  //   setPais(event.target.value)
-  // }
-
-  const fazerLogin = (event) => {
-    event.preventDefault()
-
-    // const body = {
-    //   email: email,
-    //   password: senha
-    // }
-   console.log (form)
-   cleanFields();
-  }
-
   return (
     <div>
-      <h1>Login</h1>
-      <form onSubmit={fazerLogin}>
-        <input
-          type="email"
-          value={form.email}
-          onChange={onChange}
-          placeholder={"email"}
-          required
-        />
-        <input
-          type="password"
-          value={form.password}
-          onChange={onChange}
-          placeholder={"senha"}
-          pattern={"^.{6,}"}
-          title={"sua senha dever ter no mínimo 6 caracteres"}
-          required
-        />
-        {/* <input
-        placeholder={"pais"}
-        value={pais}
-        onChange={mudaPais}
-        pattern={"[A-Z]{3}"}
-        title={'código do país com 3 letras maiúscula'}
-        /> */}
-         
-        <button>Fazer Login</button>
-      </form>
+      <HomePage />
     </div>
   );
-};
+}
 
 export default App;
---
+++ projeto-labex/viagens/src/pages/HomePage.js
@@ -0,0 +1,16 @@
+import './App.css';
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+import axios from 'axios';
+
+export default function HomePage() {
+    return (
+        <div>
+            <h1>Labex</h1>
+            <button>Viagens</button>
+            <button>Administrador</button>
+        </div>
+    )
+}
+
+export default App;
