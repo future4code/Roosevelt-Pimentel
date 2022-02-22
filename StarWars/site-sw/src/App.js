2c6e700ed30beebeff558df400536f333e88e7c1
RooseveltAndrade <rooseveltandrade14@gmail.com>
2022-02-21 22:24:07 -0300

    inicio do projeto

---
+++ StarWars/site-sw/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import CharacterDetailPage from "./pages/CharacterDetailPage/CharacterDetailPage";
+import CharacterListPage from "./pages/CharacterListPage/CharacterListPage";
+
+
+function App() {
+  return (
+    <div>
+      <CharacterDetailPage/>
+      <CharacterListPage/>
+    </div>
+  );
+}
+
+export default App;
